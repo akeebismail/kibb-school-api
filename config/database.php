93c00277f4facaa5fbb4526bf40a898b2d5aa1ad
Akeeb Ismail <damiz.akeeb@gmail.com>
2018-03-16 17:40:12 +0100

    change db

--- config/database.php
+++ config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'kibb-school'),
+            'username' => env('DB_USERNAME', 'damizkibb'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
